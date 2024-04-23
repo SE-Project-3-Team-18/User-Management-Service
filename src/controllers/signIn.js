88e3ea2a12195b644610deb9140470a253ac7d1c
Abhinav Reddy <abhinav.boddu@students.iiit.ac.in>
2024-04-23 12:14:50 +0530

    now fetching sellerId if he is a seller, in signIn

--- src/controllers/signIn.js
+++ src/controllers/signIn.js
@@ -2,7 +2,35 @@ const User = require('../models/User');
 const { CustomError } = require('../utils/error');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const axios = require('axios');
 const { jwtSecret, jwtExpiresIn } = require('../config/auth');
+const ServiceRegistryClient = require('../utils/serviceRegistry');
+
+const getSellerId = async (userId) => {
+  try {
+    const baseUrl = await ServiceRegistryClient
+      .getInstance()
+      .getUrl('Seller')
+    const url = new URL(`/api/seller-by-uid/${userId}`, baseUrl).toString()
+
+    const response = await axios.get(url, {
+      userId,
+    })
+    return response.data.sellerId
+  } catch (e) {
+    let error = null
+    if (axios.isAxiosError(e) === true) {
+      if (e.response) {
+        error = new CustomError(e.response?.data?.message, e.response?.status, false)
+      } else {
+        error = new CustomError(`Axios Error: ${e.message}`, 500, true)
+      }
+    } else {
+      error = new CustomError(e.message, 500, true)
+    }
+    throw error
+  }
+}
 
 const signIn = async (req, res, next) => {
   try {
@@ -31,9 +59,14 @@ const signIn = async (req, res, next) => {
       throw new CustomError('Invalid credentials', 401, false);
     }
 
+    let sellerId
+    if (user.role === 'seller') {
+      sellerId = await getSellerId(user.id)
+    }
     const tokenData = {
       userId: user.id,
       role: user.role,
+      sellerId,
       email: user.email,
       name: user.name,
       activated: user.activated,
